f5455f6837edb430067c3dc705e51b531673d4ae
shule517 <shule517@gmail.com>
2017-03-15 20:35:14 +0900

    チャンネルモデルの追加

---
+++ db/migrate/20170315113427_create_channels.rb
@@ -0,0 +1,13 @@
+class CreateChannels < ActiveRecord::Migration[5.0]
+  def change
+    create_table :channels do |t|
+      t.string :name
+      t.string :contact_url
+      t.string :last_genre
+      t.string :last_detail
+      t.date :last_started_at
+
+      t.timestamps
+    end
+  end
+end
