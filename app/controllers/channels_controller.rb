9e203b1447ceb8fe47c2ec8ccebe94f2fade5a2d
shule517 <shule517@gmail.com>
2017-03-17 00:15:05 +0900

    チャンネル名検索できるようにした。

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,13 @@
 class ChannelsController < ApplicationController
+  def index
+    @channels = Channel.where(name: user_params[:name])
+    render json: @channels
+  end
+
+  private
+
+  # リクエストパラメータのバリデーション
+  def user_params
+    params.permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :channels, format: 'json' do
+    get '/' , action: 'index'
+  end
+  root 'channels#index'
 end
