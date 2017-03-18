5b95de9e8822906ed6bce179cc2163f7bdba85df
shule517 <shule517@gmail.com>
2017-03-18 15:31:27 +0900

    コメントがない場合のテストケースを追加

--- spec/yp_scraping_spec.rb
+++ spec/yp_scraping_spec.rb
@@ -17,14 +17,26 @@ describe YPScraping do
   describe '配信の詳細が取得できること' do
     let(:history) { YPScraping.new(history_url) }
     context 'TPの場合' do
-      let(:history_url) { 'http://temp.orz.hm/yp/getgmt.php?cn=%E3%81%97%E3%81%A3%E3%81%8B%E3%82%8A%E3%82%B7%E3%83%A5%E3%83%BC%E3%83%AB%EF%BD%83%EF%BD%88' }
-      let(:detail) { history.detail('2016/12/09') }
-      it { expect(detail[:start_time]).to eq '19:10' } # 配信開始時間
-      it { expect(detail[:end_time]).to eq '23:50' } # 配信終了時間
-      it { expect(detail[:genre]).to eq 'プログラミング' }
-      it { expect(detail[:detail]).to eq 'rails初心者が21:00までに Pecaハッカソン用ページをなんとしてでも作る！' }
-      it { expect(detail[:contact_url]).to eq 'http://jbbs.shitaraba.net/bbs/read.cgi/game/45037/1481031910/' }
-      it { expect(detail[:comment]).to eq '21:00から開会式配信はっじまるよー　今日から3日間はPeerCast忘年会ハッカソン！' }
+      context '通常' do
+        let(:history_url) { 'http://temp.orz.hm/yp/getgmt.php?cn=%E3%81%97%E3%81%A3%E3%81%8B%E3%82%8A%E3%82%B7%E3%83%A5%E3%83%BC%E3%83%AB%EF%BD%83%EF%BD%88' }
+        let(:detail) { history.detail('2016/12/09') }
+        it { expect(detail[:start_time]).to eq '19:10' } # 配信開始時間
+        it { expect(detail[:end_time]).to eq '23:50' } # 配信終了時間
+        it { expect(detail[:genre]).to eq 'プログラミング' }
+        it { expect(detail[:detail]).to eq 'rails初心者が21:00までに Pecaハッカソン用ページをなんとしてでも作る！' }
+        it { expect(detail[:contact_url]).to eq 'http://jbbs.shitaraba.net/bbs/read.cgi/game/45037/1481031910/' }
+        it { expect(detail[:comment]).to eq '21:00から開会式配信はっじまるよー　今日から3日間はPeerCast忘年会ハッカソン！' }
+      end
+      context 'コメントがない場合' do
+        let(:history_url) { 'http://temp.orz.hm/yp/getgmt.php?cn=%E3%81%8E%E3%82%82' }
+        let(:detail) { history.detail('2017/03/17') }
+        it { expect(detail[:start_time]).to eq '21:50' } # 配信開始時間
+        it { expect(detail[:end_time]).to eq '23:50' } # 配信終了時間
+        it { expect(detail[:genre]).to eq 'game' }
+        it { expect(detail[:detail]).to eq 'ゼルダの伝説BOTW' }
+        it { expect(detail[:contact_url]).to eq 'http://jbbs.shitaraba.net/bbs/read.cgi/internet/4074/1481269391/l50' }
+        it { expect(detail[:comment]).not_to eq nil }
+      end
     end
     context 'SPの場合' do
       let(:history_url) { 'http://bayonet.ddo.jp/sp/getgmt.php?cn=%E4%B8%AD%E5%B1%B1' }
