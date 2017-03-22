9ad9b8e31f1ff74cc3bd825bc693c3a5c58c1f9d
shule517 <shule517@gmail.com>
2017-03-22 20:08:34 +0900

    配信履歴の配信者名指定検索ができるようにした

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,14 +1,20 @@
 class HistoriesController < ApplicationController
   def index
+    name = user_params[:name]
     keyword = user_params[:keyword]
-    @histories = History.where("name like '\%#{keyword}\%' or genre like '\%#{keyword}\%' or detail like '\%#{keyword}\%' or comment like '\%#{keyword}\%'").order(:end_time).reverse
-    render json: @histories
+    if name.present?
+      @histories = History.where(name: name).order(:end_time).reverse
+      render json: @histories
+    else
+      @histories = History.where("name like '\%#{keyword}\%' or genre like '\%#{keyword}\%' or detail like '\%#{keyword}\%' or comment like '\%#{keyword}\%'").order(:end_time).reverse
+      render json: @histories
+    end
   end
 
   private
 
   # リクエストパラメータのバリデーション
   def user_params
-    params.permit(:keyword)
+    params.permit(:keyword, :name)
   end
 end
